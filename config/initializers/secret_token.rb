e3c3d0c9b98bbfc4c12cbec18fe5b67b278ded0c
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-01-17 16:43:31 +0100

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiWareIdm::Application.config.secret_token = 'a8f9833fd1eb6ca33f06078286d5c526d4097a0d2ffcd7ecd90da1a8fcfeee7604fdeb5f6f4e0e92412d42c4f6a326a947aada82f02ebeda6f69133e51414e81'
