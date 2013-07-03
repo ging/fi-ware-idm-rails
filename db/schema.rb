b85449007cb52f5b22013a9096ad0a4d375a9407
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-07-03 19:15:17 +0200

    Add missing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130624143908) do
+ActiveRecord::Schema.define(:version => 20130703170407) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -177,6 +177,8 @@ ActiveRecord::Schema.define(:version => 20130624143908) do
     t.string   "notified_object_type"
     t.string   "notification_code"
     t.string   "attachment"
+    t.boolean  "global",               :default => false
+    t.datetime "expires"
   end
 
   add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
@@ -241,7 +243,7 @@ ActiveRecord::Schema.define(:version => 20130624143908) do
     t.integer  "receiver_id"
     t.string   "receiver_type"
     t.integer  "notification_id",                                  :null => false
-    t.boolean  "read",                          :default => false
+    t.boolean  "is_read",                       :default => false
     t.boolean  "trashed",                       :default => false
     t.boolean  "deleted",                       :default => false
     t.string   "mailbox_type",    :limit => 25
