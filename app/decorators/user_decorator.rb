097cacb0dc96b97cec84d43909e52c4033d7212f
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-07-29 15:03:27 +0200

    Export user roles in API

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,11 +1,3 @@
-User.class_eval do
-  # Overwrite User#represented method
-  #
-  # Right now, we do not need confirmation from users to represent
-  def represented
-    contact_subjects(direction: :received) do |q|
-      q.joins(sent_ties: { relation: :permissions }).
-        merge(Permission.represent)
-    end
-  end
-end
+require_dependency 'fi_ware_idm/models/user'
+
+User.send :include, FiWareIdm::Models::User
