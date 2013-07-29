13d0ccf56085cdcd53e95e55db643a7cf8a3375a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-07-29 12:56:18 +0200

    Rearrange method in FiWareIdm::Models::Tie module

--- lib/fi_ware_idm/models/tie.rb
+++ lib/fi_ware_idm/models/tie.rb
@@ -4,15 +4,15 @@ module FiWareIdm
       extend ActiveSupport::Concern
 
       included do
-        def relation_belongs_to_sender_with_get?
-          relation_belongs_to_sender_without_get? ||
-            relation_got_by_sender?
-        end
-
         alias_method_chain :relation_belongs_to_sender?, :get
       end
 
       private
+
+      def relation_belongs_to_sender_with_get?
+        relation_belongs_to_sender_without_get? ||
+          relation_got_by_sender?
+      end
 
       def relation_got_by_sender?
         sender.obtained_roles.include? relation
