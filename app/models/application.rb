a168cde23e3f27930fdeb1ed4c19f53c0d5f23bc
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-08-07 17:57:57 +0200

    Fix scope

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,9 +10,10 @@ class Application < Site::Client
     where(official: true)
   }
 
-  scope :purchased_by, -> (actor) {
+  scope :purchased_by, ->(actor) {
     select("DISTINCT sites.*").
       joins(actor: :sent_relations).
+      merge(Contact.received_by(actor)).
       merge(::Relation.where(id: ::Relation::Purchaser.instance.id))
   }
 
