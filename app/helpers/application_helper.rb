e48c86f62f43cd587c9d3833bdc8bdddce7872f5
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-10-02 12:32:19 +0200

    Multi-domain fiware toolbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def fiware_env_url(options = {})
+    domain = request.host.gsub(/\Aaccount\./, '')
+
+    if options[:subdomain].present?
+      domain = "#{ options[:subdomain] }.#{ domain }"
+    end
+
+    protocol = 'http'
+
+    if options[:https].present?
+      protocol += 's'
+    end
+
+    "#{ protocol }://#{ domain }"
+  end
 end
