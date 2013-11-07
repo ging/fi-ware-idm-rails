b24e313365e5f99038da94168175313b111c5f26
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-11-07 17:09:05 +0100

    Update email validation message

--- lib/fi_ware_idm/models/user.rb
+++ lib/fi_ware_idm/models/user.rb
@@ -6,9 +6,13 @@ module FiWareIdm
       included do
         validates :email,
                   format: {
-                    with: /.*@#{ FiWareIdm.allowed_email_domains.join('|') }/,
+                    with: /(#{ FiWareIdm.allowed_email_domains.join('|') })$/,
                     if: Proc.new { FiWareIdm.allowed_email_domains.present? },
-                    message: "domain you have provided is not on the list of allowed partners (PPP member institutions). Please ensure that you supply a corporate address. Failing this, contact fiware-testbed-help@lists.fi-ware.eu and an administrator will add your domain to the list" 
+                    message: "domain is not valid. Please note that you are signing up to the FI-WARE Testbed, restricted to PPP members. If you are a PPP member, please use your corporate email (not gmail, yahoo, etc.). If you are not a PPP member, you can apply for an account in FI-LAB, that is suitable for anyone, PPP member or not.
+                    
+                    If you are a PPP member using your corporate e-mail address and you get this message, please contact the support team at fiware-testbed-help@lists.fi-ware.eu to add your domain name to our white list
+        
+        Important: a given domain on the white list authorises e-mail adresses in all the subdomains under it." 
                   }
 
         # Overwrite User#represented method
