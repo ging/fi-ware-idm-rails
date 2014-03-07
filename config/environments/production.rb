7e56b019b176749e773b4a1fafdc8e4e1315e9f1
ebarra <ebarra@dit.upm.es>
2014-03-07 12:58:34 +0100

    raise delivery errors false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ FiWareIdm::Application.configure do
   config.assets.precompile += %w( signOut.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
