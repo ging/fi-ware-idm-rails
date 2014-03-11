2a375e4bb2e4b107ca69e2f2365773094bbc5d07
Gabriel Hao <gabriel.hao.wei@gmail.com>
2014-03-11 15:47:18 +0100

    add Organization file and implemented info stucture needed by SCIM

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,7 +5,34 @@ class Organization < Group
 		{
 			schemas: ["urn:scim:schemas:core:2.0:Group"],
 			id: actor.id
+			displayName: CGI::escapeHTML(name),
+
+			#multiple members needs to loop all the memebers
+			members: 
+				list_users
+			,
+			meta:{
+				resourceType: "Group",
+				created: created_at,
+				lastModified: updated_at,
+				version: "1",
+				location: controller.request.url
+			}
 		}
+	end
+
+	def list_users
+		users = []
+		self.users.each do |user|
+			users.push({
+				vale:user.id,
+				#correct?
+				"$ref": controller.request.url
+				display: CGI::escapeHTML(name)
+
+			})
+		end
+		users
 	end
 	
 end
