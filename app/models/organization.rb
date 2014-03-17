1ce969c051827996305d6b965d54c5394859709e
Aldo Gordillo <agordillos@gmail.com>
2014-03-17 13:10:12 +0100

    Comment wrong lines in Organization model, allowing to start IDM

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,13 +4,9 @@ class Organization < Group
 	def as_scim_json(version,controller)
 		{
 			schemas: ["urn:scim:schemas:core:2.0:Group"],
-			id: actor.id
+			id: actor.id,
 			displayName: CGI::escapeHTML(name),
-
-			#multiple members needs to loop all the memebers
-			members: 
-				list_users
-			,
+			# members: list_users,
 			meta:{
 				resourceType: "Group",
 				created: created_at,
@@ -22,17 +18,16 @@ class Organization < Group
 	end
 
 	def list_users
-		users = []
-		self.users.each do |user|
-			users.push({
-				vale:user.id,
-				#correct?
-				"$ref": controller.request.url
-				display: CGI::escapeHTML(name)
-
-			})
-		end
-		users
+		# users = []
+		# self.users.each do |user|
+		# 	users.push({
+		# 		vale:user.id,
+		# 		#correct?
+		# 		"$ref": controller.request.url,
+		# 		display: CGI::escapeHTML(name)
+		# 	})
+		# end
+		# users
 	end
 	
 end
