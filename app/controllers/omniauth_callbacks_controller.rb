50cddb001fff69b943d5736d8778d032d2f2f9aa
Mirko Ardinghi <mirko.ardinghi@resiltech.com>
2014-07-23 14:57:38 +0200

    New email management for external users
    Signed-off-by: Mirko Ardinghi <mirko.ardinghi@resiltech.com>

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -42,18 +42,18 @@ private
     # Removed callback path
     info_idp = ExternalIdp.find_by_route(requestVar.env['REQUEST_URI'].to_s.sub('/callback',''))
     if !info_idp.blank?
-
       if info_idp[:enabled]
-
+        logger.tagged("MIRKO") {logger.warn "enabled" }
         begin
-          mod_email = requestVar.env['omniauth.auth']['info']['email'] + "_" + info_idp[:mark]
+          email = requestVar.env['omniauth.auth']['info']['email']
         rescue    
-          mod_email = nil
+          email = nil
         end
-        
-        if !mod_email.nil?
-          if authorized_external_user?(mod_email, requestVar.env['omniauth.auth']['info']['name'], info_idp[:id])
-            sign_in User.find_by_email(mod_email)
+
+        if !email.nil?
+          id_user = authorized_external_user(email, requestVar.env['omniauth.auth']['info']['name'], info_idp[:id])
+          if id_user > 0
+            sign_in User.find(id_user)
             set_flash_message :notice, :success, :kind => info_idp[:description]
           end
         end
@@ -61,27 +61,26 @@ private
     end
   end
   
-  def authorized_external_user?(email, name, idp_mark)
+  def authorized_external_user(email, name, idp_mark)
     
-    user = User.find_by_email(email)
-    
-    # If the user not exist, create it!
-    if user.nil?
-      password = Devise.friendly_token.first(12) # Generic password
-      user = User.new      
-      user.email = email
-      user.password = password
-      user.password_confirmation = password
-      user.skip_confirmation!
-      user.name = name
-      user.ext_idp = idp_mark 
-      
-      return user.save
-    else
-      return true 
+    # check if the user already exists
+    user = User.find(:first, :include => :actor, :conditions => {'actors.email' => email, :ext_idp => idp_mark})
+    if !user.nil? # Find it!
+      return user.id
     end
+
+    password = Devise.friendly_token.first(12) # Generic password
+    user = User.new      
+    user.email = email
+    user.password = password
+    user.password_confirmation = password
+    user.skip_confirmation!
+    user.name = name
+    user.ext_idp = idp_mark 
+      
+    user.save
+    return user.id
     
-    return false
   end
   
 end
